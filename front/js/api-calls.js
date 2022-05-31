6f87c25894b3495022b53fdcaa4214194bbbfa18
Clement BELLIER <clement.bellier@gmail.com>
2022-05-31 22:46:50 +0200

    Display error messages and empty inputs messages WIP - Post Methode Order to API

--- front/js/api-calls.js
+++ front/js/api-calls.js
@@ -1,7 +1,18 @@
-const retrieveProductData = async (productId) => fetch(`http://localhost:3000/api/products/${productId}`)
+const retrieveProductData = async (productId) =>
+        fetch(`http://localhost:3000/api/products/${productId}`)
         .then(res => res.json())
         .then(data => data)
-        .catch(error =>errorManangement(error))
+        .catch(error =>errorManangement(error));
 
+const postOrderToAPI = async (orderBody) =>
+        fetch("http://localhost:3000/api/products/order", {
+                method: "POST",
+                headers: { 
+                        'Accept': 'application/json', 
+                        'Content-Type': 'application/json' 
+        },
+                body: JSON.stringify(orderBody)
+        });
 
 export {retrieveProductData}
+export {postOrderToAPI}
--- front/js/cart-order.js
+++ front/js/cart-order.js
@@ -1,4 +1,5 @@
 import { readInput } from "./productPage-addToCart.js"
+import { postOrderToAPI } from "./api-calls.js"
 
 const cartInputsNames = ["firstName","lastName","address","city","email"]
 
@@ -9,6 +10,15 @@ const cartErrorMessages = {
     city: "La ville ne doit pas contenir de caractères spéciaux excepté ',-. ",
     email: "L'email doit être dans le format suivant exemple@domaine.fr"
 }
+
+const cartEmptyMessages = {
+    firstName: "Le prénom est un champ obligatoire. Veuillez le renseigner (ex: Obi-Wan).",
+    lastName: "Le nom est un champ obligatoire. Veuillez le renseigner (ex: Kenobi).",
+    address: "L'adresse est champ obligatoire. Veuillez le renseigner (ex: 3bis, Mer de Sable).",
+    city: "La ville est un champ obligatoire. Veuillez le renseigner (ex: 1977 Mos Espa, Tatooine).",
+    email: "L'email est un champ obligatoire. Veuillez le renseigner (ex: obi-wan.kenobi@conseil-jedi.org)."
+}
+
 const formRegExp = {
     firstName: /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{1,}$/,
     lastName: /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{1,}$/,
@@ -17,10 +27,11 @@ const formRegExp = {
     email: /^[0-9a-z._-]+@{1}[0-9a-z.-]{2,}[.]{1}[a-z]{2,5}$/,
 }
 
-const listenCartInputsForHideErrorMsg = () =>{
+const listenCartInputsForErrorMsg = () =>{
     cartInputsNames.forEach(input => {
         document.querySelector(`#${input}`).addEventListener('input', ()=>{
             if(isCorrectlyFillIn(input)){
+                document.querySelector(`#${input}`).style.border = 'none'
                 document.querySelector(`#${input}ErrorMsg`).innerHTML = ''
             }
         })
@@ -28,28 +39,58 @@ const listenCartInputsForHideErrorMsg = () =>{
 }
 
 const postOrder = (orderBody) => {
-    console.log(orderBody)
+    postOrderToAPI(orderBody)
 }
 
 const buildOrderBody = (cartProductArray) => {
-    return cartProductArray
+    const firstName = readInput('#firstName')
+    const lastName = readInput('#lastName')
+    const address = readInput('#address')
+    const city = readInput('#city')
+    const email = readInput('#email')
+    const productArray = []
+    cartProductArray.forEach(product => productArray.push(product.id))
+    const orderBody = {
+        contact:{
+            firstName: firstName,
+            lastName: lastName,
+            address: address,
+            city: city,
+            email: email
+        },
+        products: productArray
+    }
+    return orderBody
 }
-const displayErrorMessage = (input) => {
-    document.querySelector(`#${input}ErrorMsg`).innerHTML = cartErrorMessages[input]
+
+const displayMessage = (input, emptyInput) => {
+    document.querySelector(`#${input}`).style.border = "3px solid #fbbcbc"
+    let cartMessages
+    if(emptyInput){
+        cartMessages = cartEmptyMessages
+    }
+    else{
+        cartMessages = cartErrorMessages
+    }
+    document.querySelector(`#${input}ErrorMsg`).innerHTML = cartMessages[input]
 }
 
 const isCorrectlyFillIn = (input) => {
     const value = readInput(`#${input}`)
+    const emptyInput = value.length === 0
+    if(emptyInput){
+        displayMessage(input, emptyInput)
+    }
     const regExp = formRegExp[input]
     if(!regExp.test(value)){
-        displayErrorMessage(input)
+        displayMessage(input, emptyInput)
         return false
     }
     return true
 }
 
 const checkInputsCorrectlyFillIn = () => {
-    if(cartInputsNames.every(input => isCorrectlyFillIn[input])
+    if(cartInputsNames.every(input => isCorrectlyFillIn(input))
       ){
           return true
       }
@@ -70,7 +111,7 @@ const listenOrderButton = (cartProductArray) => {
             order(cartProductArray)
             e.preventDefault()
         })
-    listenCartInputsForHideErrorMsg()
+    listenCartInputsForErrorMsg()
 }
 
 export {listenOrderButton}
